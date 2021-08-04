b5fe7b173d003244d91585375897584781196b13
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-04 22:24:56 +0800

    [OO Step by Step][Paraluman] feat: Practice8

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,39 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    public Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id,name,age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id,name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        String statement = super.introduce() + " I am a Teacher.";
+        if(klass == null){
+            return statement + " I teach No Class.";
+        }
+        return statement + " I teach Class " + this.klass.getNumber() + ".";
+
+    }
+
+    public String introduceWith(Student student) {
+        String statement = super.introduce() + " I am a Teacher.";
+        if(this.klass.getNumber() == student.getKlass().getNumber()){
+            return statement + " I teach " + student.getName() + ".";
+        }
+
+        return statement + " I don't teach " + student.getName() + ".";
+
+    }
+
 }
+
