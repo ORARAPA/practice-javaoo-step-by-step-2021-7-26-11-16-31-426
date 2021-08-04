23d5300ea2fca41b8f63c373aefadea300eb3c51
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-04 22:23:24 +0800

    [OO Step by Step][Paraluman] feat: Practice5

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,27 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    public int klass;
+
+    public Teacher(String name, int age) {
+        super(name,age);
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    public String introduce() {
+        String statement = super.introduce() + " I am a Teacher.";
+        if(this.klass == 0){
+            return statement + " I teach No Class.";
+        }
+        return statement + " I teach Class " + this.klass + ".";
+
+    }
 }
