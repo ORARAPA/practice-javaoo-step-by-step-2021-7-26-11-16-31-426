e552b38f866f5bad4f59097441324f8b0073136f
ORARAPA <87627137+ORARAPA@users.noreply.github.com>
2021-08-04 22:24:30 +0800

    [OO Step by Step][Paraluman] feat: Practice7

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,29 @@
 package practice07;
 
 public class Klass {
+    private int number;
+    private Object leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public Object getLeader() {
+        return leader;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + this.number;
+    }
+
+    public Object assignLeader(Student student) {
+        leader = student;
+        return leader;
+    }
+
+
 }
